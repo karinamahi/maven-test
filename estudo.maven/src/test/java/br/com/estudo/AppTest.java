808c50c6d1d4720d0f7c3bd4017f3091cf3affdd
Karina Mayumi Hirata <karinamahi@hotmail.com>
2015-03-29 13:55:53 -0300

    teste usando junit

---
+++ estudo.maven/src/test/java/br/com/estudo/AppTest.java
@@ -0,0 +1,19 @@
+package br.com.estudo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest   
+  
+{
+  
+  
+   @Test
+    public void testApp()
+    {
+       Assert.assertTrue( true );
+    }
+}
---
+++ estudo.maven2/src/test/java/br/com/estudo/AppTest.java
@@ -0,0 +1,16 @@
+package br.com.estudo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AppTest   
+
+{
+	
+	@Test
+    public void testApp()
+    {
+        Assert.assertTrue(true);
+    }
+}
